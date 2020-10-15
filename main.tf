89675fd88a25ce4c92dc8442c3249f5cf92c24fc
Gašper Armič <gasper.armic@dhimahi.com>
2020-10-15 21:24:34 +0200

    Apply a bit of formatting

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ resource "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "172.16.10.0/24"
   map_public_ip_on_launch = true
 
   tags = {
